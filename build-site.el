4aa70f2bbcf30b67c969a1aacb70461595227716
James Leslie <jamesleslie@posteo.net>
2022-02-18 15:26:12 +0000

    [updates] Added images and pdfs - updated index.org
    [typos] Fixed typo in index and added link to inftop notes
    
    [index] Updated image on index page

--- build-site.el
+++ build-site.el
@@ -103,7 +103,7 @@
 <h1 class=\"title\">%t</h1>%d")
        (list "static"
 	     :base-directory "./content"
-	     :base-extension "css\\|txt\\|jpg\\|gif\\|png\\|jpeg"
+	     :base-extension "css\\|txt\\|jpg\\|gif\\|png\\|jpeg\\|pdf"
 	     :recursive t
 	     :publishing-directory "./public"
 	     :publishing-function 'org-publish-attachment)))
