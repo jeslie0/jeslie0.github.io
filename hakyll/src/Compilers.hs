5150994545a42720d86b298685010babd9344b26
James Leslie <jamesleslie@posteo.net>
2023-07-08 19:22:31 +0100

    Fix KaTeX usage

--- hakyll/src/Compilers.hs
+++ hakyll/src/Compilers.hs
@@ -1,10 +1,37 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Compilers where
 
 import Hakyll.Core.Compiler
 import Hakyll.Core.Item
 import Hakyll.Web.Pandoc
+import Text.Pandoc.Options
 import Text.Pandoc.Shared
+
+mathExtensions :: Extensions
+mathExtensions =
+  extensionsFromList
+    [ Ext_tex_math_single_backslash, -- TeX math btw (..) [..]
+      Ext_tex_math_double_backslash, -- TeX math btw \(..\) \[..\]
+      Ext_tex_math_dollars, -- TeX math between $..$ or $$..$$
+      Ext_latex_macros, -- Parse LaTeX macro definitions (for math only)
+      Ext_inline_code_attributes, -- Ext_inline_code_attributes
+      Ext_abbreviations -- PHP markdown extra abbreviation definitions
+    ]
+
+pandocWriterOptions :: WriterOptions
+pandocWriterOptions =
+  defaultHakyllWriterOptions
+    { writerHTMLMathMethod = KaTeX ""
+    }
+
+pandocReaderOptions :: ReaderOptions
+pandocReaderOptions =
+  defaultHakyllReaderOptions
+    { readerExtensions = defaultHakyllReaderOptions.readerExtensions <> mathExtensions
+    }
 
 shiftedHeaderPandocCompiler :: Compiler (Item String)
 shiftedHeaderPandocCompiler =
-  pandocCompilerWithTransform defaultHakyllReaderOptions defaultHakyllWriterOptions (headerShift 1)
+  pandocCompilerWithTransform pandocReaderOptions pandocWriterOptions (headerShift 1)
